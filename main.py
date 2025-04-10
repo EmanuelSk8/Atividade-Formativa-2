68dae0fdc37ab177c42226397893b1d874d50b95
EmanuelSk8 <emanuel.cezar@pucpr.edu.br>
2025-04-09 21:31:57 -0300

    Cálcula o IMC

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ print("Calculadora de IMC")
 
 peso = float(input("Digite seu peso (kg): "))
 altura = float(input("Digite sua altura (m): "))
+
+imc = peso / (altura ** 2)
+print(f"Seu IMC é: {imc:.2f}")
