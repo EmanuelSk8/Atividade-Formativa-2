fa9070f9b9262bb966e2d5ab5dfc1e12c1c0fc3b
EmanuelSk8 <emanuel.cezar@pucpr.edu.br>
2025-04-29 11:48:27 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -18,7 +18,7 @@ async def test_funcaoteste():
     assert result == {"teste": True, "num_aleatorio": 12345}
 
 @pytest.mark.asyncio
-async def test_create_estudante(estudante: Estudante):
+async def test_create_estudante():
     estudante_teste = Estudante(name="Emanuel", curso ="Devops", ativo=False)
     result = await create_estudante(estudante_teste)
     assert estudante_teste == result
@@ -32,14 +32,14 @@ async def test_update_estudante_negativo():
 async def test_update_estudante_positivo():
     result = await update_estudante(10)
     assert result
-@pytest.mark.asyncio
 
-async def test_delete_estudante_positivo(id_estudante: int):
+@pytest.mark.asyncio
+async def test_delete_estudante_positivo():
     result = await delete_estudante(10)
     assert result
 
 @pytest.mark.asyncio
-async def test_delete_estudante_negativo(id_estudante: int):
+async def test_delete_estudante_negativo():
     result = await delete_estudante(-5)
     assert not result
 
