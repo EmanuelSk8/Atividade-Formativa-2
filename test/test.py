52ac90275eaf041760407c94dc46e9b22dc92987
EmanuelSk8 <emanuel.cezar@pucpr.edu.br>
2025-04-29 11:49:33 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,7 +10,6 @@ async def test_root():
     result = await root()
     assert result == {"message": "Hello World"}
 
-
 @pytest.mark.asyncio
 async def test_funcaoteste():
     with patch('random.randint', return_value=12345):
